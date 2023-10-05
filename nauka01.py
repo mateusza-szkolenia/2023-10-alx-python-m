9c2a247c769bf90730ec519df576f1fcf28157d1
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-05 19:27:09 +0200

    nauka 1

---
+++ nauka01.py
@@ -0,0 +1,37 @@
+# mój pierwszy program
+
+print("Dzien dobry")
+
+print(2 + 2)    # dodawanie
+print(55 - 30)  # odejmowanie
+print(3 * 7)    # mnożenie
+print(100 / 3)  # dzielenie (=33.333333...)
+print(100 // 3) # dzielenie całkowite (=33)
+
+# ten operator nie występuje w prawie żadnym innym języku
+# (C, C++, Java, PHP, JavaScript)
+
+print(2 ** 20)  # potęgowanie
+#print(2 ** 300)
+
+print("Xyz" + "ABC")
+
+# taka operacja jest specyficzna dla języka Python
+print(10 * "Xyz")
+
+# to nie zadziała
+# print(30 + "Abc")
+
+print("30" + "Abc")
+
+print("10" + "40")
+
+print((((4 - 2) + 1) + (2 + 1)) * 3)
+
+print(10 / 7)
+
+# dzielenie z resztą:
+# 100 : 3 = 33 r. 1
+
+print(100 // 3)
+print(100 % 3)
