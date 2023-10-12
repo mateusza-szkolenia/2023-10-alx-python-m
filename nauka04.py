121e4cf114d099b8bf8748b28a9abce8ca0a113e
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-12 19:05:03 +0200

    użycie f-stringów

--- nauka04.py
+++ nauka04.py
@@ -2,6 +2,7 @@ rok_urodzenia = input("Podaj rok urodzenia: ")
 
 wiek = 2023 - int(rok_urodzenia)
 
-komunikat = "Mam " + str(wiek) + " lat"
+# komunikat = "Mam " + str(wiek) + " lat"
+komunikat = f"Mam {wiek} lat"
 
 print(komunikat)
