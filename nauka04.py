f527917e5a4a8923524d60d0b94db33ab81d59f4
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-12 19:00:07 +0200

    konwersja liczby na napis

--- nauka04.py
+++ nauka04.py
@@ -2,8 +2,6 @@ rok_urodzenia = input("Podaj rok urodzenia: ")
 
 wiek = 2023 - int(rok_urodzenia)
 
-print(wiek)
+komunikat = "Mam " + str(wiek) + " lat"
 
-#komunikat = "Nazywam się " + imie + " i mam " + wiek + " lat"
-
-#print(komunikat)
+print(komunikat)
