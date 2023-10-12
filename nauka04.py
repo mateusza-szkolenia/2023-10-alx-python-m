d16b993832f2a4e952575d9c06c6ec5373ee104b
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-12 18:54:56 +0200

    rozwiązanie zad 1

---
+++ nauka04.py
@@ -0,0 +1,9 @@
+rok_urodzenia = input("Podaj rok urodzenia: ")
+
+wiek = 2023 - int(rok_urodzenia)
+
+print(wiek)
+
+#komunikat = "Nazywam się " + imie + " i mam " + wiek + " lat"
+
+#print(komunikat)
