7d6557a4b4d981c56ca43f205ceb93ad608a3aa7
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-19 18:52:34 +0200

    rozwiązanie

---
+++ zadanie03.py
@@ -0,0 +1,9 @@
+ROK_BIEZACY = 2023
+
+rok_ur = float(input('Podaj rok urodzenia: '))
+wiek = ROK_BIEZACY - rok_ur
+print(wiek)
+if wiek >= 18:
+    print('Jesteś pełnoletni')
+else:
+    print('Nie jestes pełnoletni')
