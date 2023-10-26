744966181ed6e43f6e19d5423bbd7d2c283f7f39
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-26 19:23:14 +0200

    operator and

--- nauka10.py
+++ nauka10.py
@@ -1,11 +1,6 @@
 slowo = input("Podaj słowo zawierające literę a oraz dłuższe niż 5 liter: ")
 
-if 'a' in slowo:
-    print("Zawiera A")
+if 'a' in slowo and len(slowo) > 5:
+    print("Zawiera A i ma wystarczającą długość")
 else:
-    print("Źle! Brak litery A")
-
-if len(slowo) > 5:
-    print("Prawidłowa długość")
-else:
-    print("Za krótkie")
+    print("Źle! (Brak litery A albo za krótkie)")
