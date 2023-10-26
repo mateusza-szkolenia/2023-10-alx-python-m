550875bbe4095ce0ff071198510e389377164664
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-26 17:54:22 +0200

    zmiana jednostki na metry

--- zadanie05.py
+++ zadanie05.py
@@ -3,10 +3,10 @@
 # BMI = masa / wzrost^2 [kg / m^2]
 
 waga = float(input("Podaj wagę: [kg] "))
-wzrost = float(input('Podaj wzrost: [cm] '))
+wzrost = float(input('Podaj wzrost: [m] '))
 
 # wzrost = wzrost / 100
-wzrost /= 100
+# wzrost /= 100
 
 bmi = waga / (wzrost**2)
 
