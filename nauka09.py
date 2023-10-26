0ac8ca6ffd2b3b8d7aa711c623c2fb426b97a6f0
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-26 18:16:09 +0200

    różne porównania i sprawdzenia

--- nauka09.py
+++ nauka09.py
@@ -1,14 +1,30 @@
 a = 100
 b = 60
 
+waluta = "PLN"
+
 print(f'{a + b = }')
 print(f'{a > b = }')
 print(f'{a > 1000 = }')
+print(f'{waluta == "USD" = }')
+
+# wielokrotne porównanie
+# notacja specyficzna dla języka Python
+print(f'{50 < a < 200 = }')
+
+zdanie = "Ala ma kota"
+
+# print("kot" in zdanie)
+print(f'{"kot" in zdanie = }')
+print(f'{"pies" in zdanie = }')
 
 print(type("abc"))
 print(type(123))
 print(type(123.5))
 print(type(True))
+
+print(None)
+print(type(None))
 
 # Typ logiczny
 # bool
