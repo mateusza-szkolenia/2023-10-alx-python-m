73687d78ac4ca3f8908266eba3f602b3cf40d495
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-26 19:10:08 +0200

    rozwiązanie zad 5

--- zadanie05.py
+++ zadanie05.py
@@ -8,7 +8,35 @@ wzrost = float(input('Podaj wzrost: [m] '))
 # wzrost = wzrost / 100
 # wzrost /= 100
 
+"""
+Kategoria 	BMI (kg/m²)
+wygłodzenie 	< 16,0
+wychudzenie 	16,0–16,99
+niedowaga 	17,0–18,49
+pożądana masa ciała 	18,5–24,99
+nadwaga 	25,0–29,99
+otyłość I stopnia 	30,0–34,99
+otyłość II stopnia (duża) 	35,0–39,99
+otyłość III stopnia (chorobliwa) 	≥ 40,0 
+"""
+
 bmi = waga / (wzrost**2)
 
-print(f'BMI wynosi: {bmi}')
+if bmi < 16.0:
+    werdykt = 'wygłodzenie'
+elif bmi < 17.0:
+    werdykt = 'wychudzenie'
+elif bmi < 18.5:
+    werdykt = 'niedowaga'
+elif bmi < 25.0:
+    werdykt = 'OK'
+elif bmi < 30.0:
+    werdykt = 'nadwaga'
+elif bmi < 35.0:
+    werdykt = 'otylosc I stopnia'
+elif bmi < 40.0:
+    werdykt = 'otylosc II stopnia'
+else:
+    werdykt = 'otylosc III stopnia'
 
+print(f'BMI wynosi: {bmi} {werdykt}')
