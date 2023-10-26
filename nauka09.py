e258479a4e8031fd34eb719050fce10a2cef5f56
Mateusz Adamowski <m.adamowski@alx.pl>
2023-10-26 18:05:43 +0200

    typy w pythonie

---
+++ nauka09.py
@@ -0,0 +1,19 @@
+a = 100
+b = 60
+
+print(f'{a + b = }')
+print(f'{a > b = }')
+print(f'{a > 1000 = }')
+
+print(type("abc"))
+print(type(123))
+print(type(123.5))
+print(type(True))
+
+# Typ logiczny
+# bool
+# Boole -> boolean -> bool
+
+
+
+
