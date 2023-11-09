ef38fd4d32701da01ab518f98b20230d1a278d05
Mateusz Adamowski <m.adamowski@alx.pl>
2023-11-09 18:22:25 +0100

    konwersja i wyświetlanie

---
+++ nauka12.py
@@ -0,0 +1,14 @@
+cena = 23_500_000
+
+liczba = 0b_1000_1010_1010_1100
+
+print(liczba)
+
+# konwersja na string reprezentujący liczbę w postaci binarnej 0b....
+liczba_w_zapisie_bin = bin(liczba)
+
+print(liczba_w_zapisie_bin)
+print(f"binarnie: [{liczba:b}]")
+print(f"binarnie: [{liczba:20b}]")
+print(f"binarnie: [{liczba:020b}]")
+print(f"binarnie: [{liczba:<20b}]")
