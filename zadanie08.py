afc80cd374d69edb24504cfd4eac71c2ace13eb5
Mateusz Adamowski <m.adamowski@alx.pl>
2023-11-16 18:01:09 +0100

    rozwiązanie

--- zadanie08.py
+++ zadanie08.py
@@ -2,11 +2,29 @@
 
 # BMI = masa / wzrost^2 [kg / m^2]
 
-waga = float(input("Podaj wagę: [kg] "))
-wzrost = float(input('Podaj wzrost: [m] '))
+waga = input("Podaj wagę: ")
+waga_mnoznik = 1
 
-# wzrost = wzrost / 100
-# wzrost /= 100
+if waga.endswith('kg'):
+    print(f"Podałeś wagę w kilogramach: [{waga}]")
+    waga = waga.removesuffix('kg')
+elif waga.endswith('g'):
+    print(f"Podałeś wagę w gramach: [{waga}]")
+    waga = waga.removesuffix('g')
+    waga_mnoznik = 0.001
+
+waga = float(waga) * waga_mnoznik
+
+wzrost = input('Podaj wzrost: ')
+wzrost_mnoznik = 1
+
+if wzrost.endswith('cm'):
+    wzrost = wzrost.removesuffix('cm')
+    wzrost_mnoznik = 0.01
+elif wzrost.endswith('m'):
+    wzrost = wzrost.removesuffix('m')
+
+wzrost = float(wzrost) * wzrost_mnoznik
 
 bmi = waga / (wzrost**2)
 
