4620a854cc69f430109a9f3fada7701ad62eb66f
Mateusz Adamowski <m.adamowski@alx.pl>
2023-11-16 19:03:21 +0100

    rozw 1. krok

---
+++ zadanie09.py
@@ -0,0 +1,14 @@
+"""
+Napisać program, który:
+
+* zapyta użytkownika o adres email
+* "ocenzuruje" adres email,
+   tzw. przykładowy adres: `m.adamowski@alx.pl`
+   zamieni na `m__________@a_____` (liczba podkreśleń musi się zgadzać)
+
+"""
+
+email = input("Podaj email: ")
+
+print(f"Twój email to: {email}")
+
