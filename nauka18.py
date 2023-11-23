ed1ed36f0efc4708bd415134df6edea1aa021ac5
Mateusz Adamowski <m.adamowski@alx.pl>
2023-11-23 17:57:01 +0100

    listy

---
+++ nauka18.py
@@ -0,0 +1,15 @@
+# typy złożone
+
+osoby = [
+    'Mateusz',
+    "Magda",
+    'Dariusz',
+    "Tom" + "asz",
+    'Maciej',
+    'Andrzej'
+]
+
+print(osoby[0])     # Mateusz
+print(osoby[3])     # Tomasz
+print(osoby[-1])    # Andrzej
+print(osoby[1:4])   # ['Magda', 'Dariusz', 'Tomasz']
