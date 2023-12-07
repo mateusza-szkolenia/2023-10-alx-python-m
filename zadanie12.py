e065c7329e3c62d80e4da060e6c6c4a994a02217
Mateusz Adamowski <m.adamowski@alx.pl>
2023-12-07 19:25:32 +0100

    trójkąt

--- zadanie12.py
+++ zadanie12.py
@@ -1,4 +1,15 @@
 # trójkąt
 
-for i in range(1, 11):
-    print(i * '#')
+"""
+                 numer linii     liczba_kropek       liczba gwiazdek
+.......*            0               7                   1
+......***           1               6                   3
+.....*****          2               5                   5
+....*******         3               4                   7
+...*********        4               3                   9
+
+                    n               7-n                 2*n + 1
+"""
+
+for n in range(5):
+    print((7 - n) * '.' + (2 * n + 1) * "*" )
