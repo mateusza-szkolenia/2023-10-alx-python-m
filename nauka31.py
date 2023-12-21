cbe50d3dd4fb288bfff5fef6c2556e7a30aac9b5
Mateusz Adamowski <m.adamowski@alx.pl>
2023-12-21 19:06:53 +0100

    zwięzły zapis

--- nauka31.py
+++ nauka31.py
@@ -2,16 +2,12 @@ import urllib.request
 import json
 # import requests
 
-url = 'http://api.nbp.pl/api/exchangerates/rates/A/USD/2020-05-11/?format=json'
+url = 'http://api.nbp.pl/api/exchangerates/rates/A/EUR/today/?format=json'
 
 with urllib.request.urlopen(url) as odpowiedz_serwera:
-    wynik = odpowiedz_serwera.read()
+    wynik = json.load(odpowiedz_serwera)
 
 print(wynik)
 
-wynik2 = json.loads(wynik)
-
-print(wynik2)
-
-kurs = wynik2['rates'][0]['mid']
+kurs = wynik['rates'][0]['mid']
 print(kurs)
