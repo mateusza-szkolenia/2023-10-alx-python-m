0fa4531308b21631a56496881bd23d6ac34c3485
Mateusz Adamowski <m.adamowski@alx.pl>
2023-12-21 18:21:52 +0100

    zadanie 14 - bisekcja

---
+++ zadanie14.py
@@ -0,0 +1,25 @@
+# zapis specyficzny dla pythona
+dol, gora = 0, 1000000
+
+print(f"Wymyśl liczbę z zakresu {dol}..{gora}")
+input("Naciśnij enter...")
+
+licznik = 0
+
+while True:
+    licznik += 1
+    liczba = (dol + gora) // 2
+    print(f"Czy jest to {liczba}?")
+    odp = input("Odp [+ - =]: ")
+    if odp == "+":
+        dol = liczba
+        continue
+    if odp == "-":
+        gora = liczba
+        continue
+    if odp == "=":
+        print("Hurra! Zgadłem!")
+        break
+    print("NIE ROZUMIEM")
+print(f"Koniec {licznik}")
+
