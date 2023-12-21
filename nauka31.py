2758fa6194e4bd7818dffe4641deac4b0ca26f40
Mateusz Adamowski <m.adamowski@alx.pl>
2023-12-21 18:57:13 +0100

    pobranie kursu

--- nauka31.py
+++ nauka31.py
@@ -7,5 +7,11 @@ url = 'http://api.nbp.pl/api/exchangerates/rates/A/USD/2020-05-11/?format=json'
 with urllib.request.urlopen(url) as odpowiedz_serwera:
     wynik = odpowiedz_serwera.read()
 
-
 print(wynik)
+
+wynik2 = json.loads(wynik)
+
+print(wynik2)
+
+kurs = wynik2['rates'][0]['mid']
+print(kurs)
