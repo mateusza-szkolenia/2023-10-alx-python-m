14a23e5b9cce44bf36d0a6f22f54f34e54db0931
Mateusz Adamowski <m.adamowski@alx.pl>
2024-01-04 18:11:10 +0100

    unikalne

--- nauka32.py
+++ nauka32.py
@@ -1,5 +1,14 @@
 liczby = [4, 5, 7, 10, 3, 5, 5, 6, 5, 5, 4, 4, 10, 1, 3, 3, 3, 3]
 
+# wynik: [4, 5, 7, 10, 3, 6, 1]
 # wypisać unikalne liczby (bez powtórzeń)
+# (usunąć duplikaty)
 # (oraz ile ich jest)
 
+unikalne = []
+
+for liczba in liczby:
+    if liczba not in unikalne:
+        unikalne.append(liczba)
+
+print(unikalne)
